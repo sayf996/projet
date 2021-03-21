3f16a3c9989333f0aa4f9ac82c184aa6a3347d82
Saif MAJID <saifuldeen.majid@etud.univ-jfc.fr>
2021-03-21 01:29:28 +0100

    Added Comments

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ const productQte = document.getElementById("quantity");
 const url =
   "https://webmmi.iut-tlse3.fr/~jean-marie.pecatte/frigo/public/31/produits";
 
+// undefined variable to use it to render Products in table
 let output = "";
+
+//Headers for the Table
 
 let headers = `<tr>
   <th>ID</th>
   <th>Product Name</th>
   <th>Quantity</th>
   </tr>`;
+
+
+//Function renderProducts to put products in table and Push it in HTML
 
 function renderProducts(products) {
   products.forEach((product) => {
@@ -34,11 +40,17 @@ function renderProducts(products) {
   productsList.innerHTML = headers + output;
 }
 
+
+
 // Get - Read the products
 // Method: GET
 fetch(url)
   .then((res) => res.json())
   .then((data) => renderProducts(data));
+
+
+
+  //Function add to add a new product used for add button
 
 function add() {
   fetch(url, {
@@ -60,6 +72,9 @@ function add() {
     .then(() => location.reload());
 }
 
+
+// function deleteItem to delete product used for delete Button
+
 function deleteItem(id) {
   fetch(`${url}/${id}`, {
     method: "DELETE",
@@ -68,11 +83,19 @@ function deleteItem(id) {
     .then(() => location.reload());
 }
 
+
+
+// Copy the API Json to Array in JS so we can use it in next functions
+
 let jsonResult = "";
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => (jsonResult = data));
+
+
+
+  // function renderResult to render table with buttons in HTML for searcnKeyword function 
 
 let searchResult = "";
 const resultDiv = document.querySelector(".search-res");
@@ -93,10 +116,14 @@ function renderResult(product) {
   resultDiv.innerHTML = headers + searchResult;
 }
 
+
+// function to search for keyword in Json table used in Search Button
 function searchKeyword() {
   let resultExists = false;
   searchResult = "";
   let searchvalue = document.getElementById("search-value").value;
+
+// For loop to see if Keyword entered in search value exists in our Json Table
 
   for (let i = 0; i < jsonResult.length; i++) {
     if (jsonResult[i].nom.toLowerCase().includes(searchvalue.toLowerCase())) {
@@ -104,10 +131,16 @@ function searchKeyword() {
       resultExists = true;
     }
   }
+
+
+  // If result wasnt found or no data entered in input 
   if (searchvalue.length == 0 || resultExists === false)
     resultDiv.innerHTML = `<td>No Result</td>`;
 }
 
+
+
+// renderCheck function is render result table after checking in checkId()  
 let checkResult = "";
 let checkTable = document.getElementById("check-res");
 
@@ -118,10 +151,13 @@ function renderCheck(products) {
   checkTable.innerHTML = headers + checkResult;
 }
 
+// function to check if product exist by Id used in Check Button
 function checkID() {
   let checkValue = document.getElementById("check-value").value;
   let statusIndi = document.getElementById("statusIndi");
   let checkExists = false;
+
+// For loop to render table for matched ID and to make the status indicator Green
 
   for (let i = 0; i < jsonResult.length; i++) {
     if (jsonResult[i].id == checkValue) {
@@ -130,21 +166,32 @@ function checkID() {
       checkExists = true;
     }
   }
+
+// If no ID match in checkID() makes the status indicator Red 
   if (!checkExists) {
     statusIndi.style.backgroundColor = "red";
     checkTable.innerHTML = `<td>No Result</td>`;
   }
+
+//If no Input entered makes indicator back to Grey
   if (checkValue == 0) {
     statusIndi.style.backgroundColor = "grey";
     checkTable.innerHTML = `<td>Please enter a value</td>`;
   }
 }
 
+
+//updateItem(id) used for Modify Button
+
 let inputValueNom = "";
 let inputValueQte = "";
 function updateItem(id) {
   let pNom = document.querySelector(".product-nom" + id);
   let pQte = document.querySelector(".product-qte" + id);
+
+
+  // Making input tags for Qte and Nom of the pressed Btn ID
+
   inputValueNom =
     `<input type="text" class="nom${id}" value="` +
     pNom.textContent +
@@ -153,10 +200,16 @@ function updateItem(id) {
     `<input type="number" class="qte${id}" value="` +
     pQte.textContent +
     `"></input>`;
+
+    //Create Cancel Button and Save Button
+
   let createBtns = `<button class= "button button7" onclick="cancelBtn()">✖</button> <button class="button button3" id="${id}"onclick="saveBtn(this.id)">Save</button>`;
   let updateBtnTD = document.querySelector(".update-btn" + id);
   pNom.innerHTML = inputValueNom;
   pQte.innerHTML = inputValueQte;
+
+
+// Hide Modify Button onClick and add the other 2 buttons
 
   document.addEventListener("click", function (e) {
     if (e.target && e.target.id == id) {
@@ -165,7 +218,8 @@ function updateItem(id) {
     }
   });
 }
-// Method: "PUT" Doesnt Work
+
+// Method: "PUT" used for Save Button 
 function saveBtn(id) {
   const updatedInputNom = document.querySelector(".nom" + id).value;
   const updatedInputQte = document.querySelector(".qte" + id).value;
@@ -184,10 +238,13 @@ function saveBtn(id) {
   location.reload();
 }
 
+// CancelBtn() used for Cancel button
 function cancelBtn() {
   location.reload();
 }
 
+
+//Function to delete item if null after update
 function deleteIteamIfNull(JsonArray) {
   for (let i = 0; i < JsonArray.length; i++) {
     if (JsonArray[i].qte <= 0) {
